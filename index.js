541b0e83a2692aff8e0a7860e1835558da625dab
ditero <anelesigenu@gmail.com>
2018-02-13 11:52:08 +0200

    ace changes

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ const mongoURL = process.env.MONGO_DB_URL ||'mongodb://localhost/backend-log';
 
 // REQUIRE OTHER FILES HERE
 const model = require('./modules/Modules');
-const routes = require('./routes/Router');
+
 // INITILIASE
 const Model = model(mongoURL)
-const Routes = routes(Model);
 
 
 // intialise dependencies
@@ -45,6 +44,8 @@ app.use(session({
   saveUninitialized: true
 }))
 
+
+
 app.use(cors());
 
 // Get Route to get home
@@ -53,42 +54,9 @@ app.get('/', function(req, res, next) {
 });
 
 // OTHER ROUTES GO HERE
-app.get('/manager',Routes.manager);
 
 //Post Route to login
-app.post('/login', function(req, res, next){
-  var users = {
-    "developer": "developer",
-    "manager": "manager",
-    "picker":"picker"
-  };
-
-  var username = req.body.username;
-
-  var userRole = users[username];
-
-  if (userRole && req.body.password === 'steltixE1') {
-    req.session.username = req.body.username;
-    req.session.userRole = userRole;
-
-    if (userRole == 'manager') {
-      //Redirect the routing to Manager VIEW
-      res.redirect('managerView' + username)
-      
-    }else if (userRole == 'picker') {
-      //Redirect the routing to Pickwe View
-      res.redirect('pickerView' + username)
-      
-    }else if (userRole == 'developer') {
-            //Redirect the routing to Pickwe View
-            res.redirect('manager')
-    }
-    
-  }
-
-  
-
-});
+app.post('/login', );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
